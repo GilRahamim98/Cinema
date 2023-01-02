a335bba093ac247c512a4d029c6bc070f00076c3
GilRahamim98 <95537455+GilRahamim98@users.noreply.github.com>
2023-01-02 17:35:49 +0200

    Update database.js

--- backend/subscriptionsWS/config/database.js
+++ backend/subscriptionsWS/config/database.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
 const connectDB = () => {
-    const uri = `mongodb+srv://Gil23:dhk123@cluster0.i9bip.mongodb.net/SubscriptionsDB?retryWrites=true&w=majority`
     const options = {
         useNewUrlParser: true,
         useUnifiedTopology: true
